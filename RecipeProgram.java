795ba7b00080f723bf1301d4201d52944d7a131c
Yuiji Imagawa <using923@gmail.com>
2014-07-06 11:44:36 +0900

    Resolve kadai3

--- RecipeProgram.java
+++ RecipeProgram.java
@@ -1,9 +1,28 @@
-public class RecipeProgram {
-	
-	public static void main(String args[]){
-		String recipe ="オムライス";
 
-		System.out.println(recipe);	
-	}
-	
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class RecipeProgram {
+
+    public static void main(String args[]) throws FileNotFoundException {
+        // レシピデータハンドリング系の変数には必ずrecipeとつける
+        String recipe;
+        String recipeFilePath = args[0];
+        File recipeFile = new File(recipeFilePath);
+        FileReader recipeFileReader = new FileReader(recipeFile);
+        BufferedReader recipeBufferedReader =
+                new BufferedReader(recipeFileReader);
+        
+        try{
+            recipe = recipeBufferedReader.readLine();
+            System.out.println(recipe);
+        } catch(Exception e){
+            e.printStackTrace();
+        }
+
+    }
+
 }
